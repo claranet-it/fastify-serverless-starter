bafcac29ca8ef92baf97617f0c02bff82770f222
Massimo Biagioli <biagiolimassimo@gmail.com>
2024-06-03 16:59:08 +0200

    change health response

--- src/routes/health/index.ts
+++ src/routes/health/index.ts
@@ -2,7 +2,7 @@ import { FastifyPluginAsync } from "fastify"
 
 const health: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
-    return { status: 'ok' }
+    return { status: 'health ok' }
   })
 }
 
